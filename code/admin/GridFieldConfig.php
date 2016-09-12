3bb33c957fc94956452ef8f248a76c922b3dbb3f
Gorrie <gorriecoe@gmail.com>
2016-09-13 07:45:19 +1200

    Added multiclassrecord grid field

--- code/admin/GridFieldConfig.php
+++ code/admin/GridFieldConfig.php
@@ -12,12 +12,25 @@ class GridFieldConfig_MultiClass extends GridFieldConfig_RelationEditor
         parent::__construct();
         $this->removeComponentsByType('GridFieldAddNewButton');
         $this->addComponent(new GridFieldAddNewMultiClass());
-
+        $this->getComponentByType('GridFieldAddNewMultiClass')
+            ->setClasses($classes);
         if ($relationship->Count() > 1) {
             $this->addComponent(new GridFieldOrderableRows());
         }
+    }
+}
 
+class GridFieldConfig_MultiClassRecord extends GridFieldConfig_RecordEditor
+{
+    public function __construct($relationship, $classes)
+    {
+        parent::__construct();
+        $this->removeComponentsByType('GridFieldAddNewButton');
+        $this->addComponent(new GridFieldAddNewMultiClass());
         $this->getComponentByType('GridFieldAddNewMultiClass')
             ->setClasses($classes);
+        if ($relationship->Count() > 1) {
+            $this->addComponent(new GridFieldOrderableRows());
+        }
     }
 }
